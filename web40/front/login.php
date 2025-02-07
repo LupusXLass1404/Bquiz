7929322e84cea24d588f57275b95f81b2375f89d
LupusXLass1404 <berkasxlupusxlass@gmail.com>
2025-02-07 14:48:42 +0800

    完成登入功能

--- web40/front/login.php
+++ web40/front/login.php
@@ -19,11 +19,41 @@
     <tr>
         <td class="tt ct">驗證碼</td>
         <td class="pp">
-            <input type="text" name="ckeck" id="ckeck">
+            <?php
+                $a = rand(10, 99);
+                $b = rand(10, 99);
+
+                $_SESSION['ans'] = $a + $b;
+                echo $a . " + " . $b . " = ";
+            ?>
+            <input type="text" name="ans" id="ans">
         </td>
     </tr>
 </table>
 
 <div class="ct">
-    <button>確認</button>
+    <button onclick="login()">確認</button>
 </div>
+
+<script>
+    function login(){  
+        let ans = $('#ans').val();
+        $.get("./api/chk_ans.php", {ans}, function(res){
+            // console.log(res);
+            if(parseInt(res)){
+                $.post('./api/chk_pw.php', {
+                    acc: $('#acc').val(),
+                    pw: $('#pw').val()
+                }, function(res){
+                    if(parseInt(res)){
+                        location.href = './index.php';
+                    } else {
+                        alert('帳號或密碼錯誤。');
+                    }
+                })
+            } else {
+                alert('驗證碼錯誤，情重新輸入。');
+            }
+        })
+    }
+</script>
