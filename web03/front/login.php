cfa1d71be996381edc65844dbc53c7c6180c6f89
LupusXLass1404 <BerkasXLupusXLass@gmail.com>
2025-02-28 23:41:43 +0800

    登入。5min

--- web03/front/login.php
+++ web03/front/login.php
@@ -1,3 +1,19 @@
 <?php
-to('./admin.php');
+if(isset($_POST['acc']) && $_POST['acc'] == 'admin' && isset($_POST['pw']) && $_POST['pw'] == '1234'){
+    $_SESSION['login'] = $_POST['acc'];
+}
+
+if(isset($_SESSION['login'])){
+    to('./admin.php');
+}
 ?>
+
+<form action="?do=login" method="post" class="tab ct rb">
+    <h2 class="ct rb">管理登入</h2>
+    <p>帳號：<input type="text" name="acc" id="acc"></p>
+    <p>密碼：<input type="password" name="pw" id="pw"></p>
+    <p class="ct">
+        <input type="submit" value="登入">
+        <input type="reset" value="清除">
+    </p>
+</form>
