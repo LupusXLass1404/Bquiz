c2142b176517d5bb71475f43b5ad996c3d2e29c3
LupusXLass1404 <BerkasXLupusXLass@gmail.com>
2025-03-01 01:22:56 +0800

    預告片動畫。1hr

--- web03/front/main.php
+++ web03/front/main.php
@@ -2,18 +2,23 @@
     <h1>預告片介紹</h1>
     <div class="rb tab" style="width:95%;">
         <div id="abgne-block-20111227">
-            <div id="posters">1</div>
+            <?php $rows = $Poster->all(['sh'=>1], 'Order by `rank`'); ?>
+            <div id="posters" class="aut">
+                <?php foreach($rows as $row): ?>
+                    <div class="poster ct aut" data-ani="<?=$row['ani'];?>">
+                        <img src="./upload/<?=$row['img'];?>" alt=""  style="height:260px">
+                        <br><?=$row['name'];?>
+                    </div>
+                <?php endforeach; ?>
+            </div>
             <div id="box">
                 <div>
                     <img src="./icon/left.jpg" alt="" onclick="move('left')">
                 </div>
                 <div id="items">
-                    <?php
-                        $rows = $Poster->all(['sh'=>1], 'Order by `rank`');
-                        foreach($rows as $row):
-                    ?>
-                    <div class="item" style="padding-left: 8px">
-                        <img src="./upload/<?=$row['img'];?>" alt="" width=80px>
+                    <?php foreach($rows as $row): ?>
+                    <div class="item ct" style="padding-left: 8px">
+                        <img src="./upload/<?=$row['img'];?>" alt="" width=80px><?=$row['name'];?>
                     </div>
                     <?php endforeach; ?>
                 </div>
@@ -25,16 +30,65 @@
     </div>
 </div>
 <script>
+    let num = 0;
+    let l = $('.poster').length;
+    let int = setInterval(()=> {anishow(num)}, 3000);
+    anishow(num);
+
+    function anishow(n){
+        let now = $('.poster').eq(n);
+        let next = $('.poster').eq(n+1);
+        let ani = now.data('ani');
+
+        $('.poster').hide();
+        now.show();
+
+        switch (ani) {
+            case 2:
+                now.slideUp(1000, function(){
+                    next.slideDown(1000);
+                });
+                break;
+            case 3:
+                now.hide(1000, function(){
+                    next.show(1000);
+                });
+                break;
+            default:
+                now.fadeOut(1000, function(){
+                    next.fadeIn(1000);
+                });
+                break;
+        }
+
+        if(num < l-1){
+            num++;
+        } else {
+            num = 0;
+        }
+
+        if(int == 0){
+            int = setInterval(()=> {anishow(num)}, 1500);
+        }
+    }
+
+    $('.item').on('click', function(){
+        console.log(123);
+
+        num = $(this).index();
+        clearInterval();
+        $('.poster').stop();
+        anishow(num);
+    })
+
     let p = 0;
     let end = $('.item').length - 4;
     function move(arr){
         if(arr == 'left' && p > 0) p--;
         if(arr == 'right' && p < end) p++;
 
-        // console.log('123');
         $('.item').animate({right: p*88});
     }
-
 </script>
 
 
